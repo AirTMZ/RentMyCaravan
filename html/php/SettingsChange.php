6265754534c905d951bd2e70918c268ad11c1fcb
JessiCat04 <155186938+JessiCat04@users.noreply.github.com>
2024-04-24 12:42:54 +0100

    Created php code for fetch and settings

--- html/php/SettingsChange.php
+++ html/php/SettingsChange.php
@@ -1,11 +1,41 @@
 <?php
 session_start();
 
-$host = 'localhost';
+$servername = 'localhost';
 $dbname = 'rentmycar';
-$dbUsername = 'root';
-$dbPassword = '';
+$username = 'root';
+$password = '';
 
 try {
-    
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Updating user settings
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // $id = $_SESSION['user_id']; 
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $phone_number = $_POST['telephone'];
+        $address = $_POST['address'];
+        $password = $_POST['PASSWORD']; 
+
+        $sql = "UPDATE users SET 
+                first_name='$first_name', 
+                last_name='$last_name', 
+                email='$email', 
+                telephone='$phone_number', 
+                address='$address', 
+                password='$password' 
+                WHERE id=$id";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "User settings updated successfully";
+        } else {
+            throw new Exception("Error updating settings: " . $conn->error);
+        }
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
+?>
